60ef8b360fe9b054320382f37bf4fd5d0e7173e4
bangelua <bangelua@gmail.com>
2018-11-28 17:04:54 +0800

    update at 11-28

--- wifiAdb.sh
+++ wifiAdb.sh
@@ -2,7 +2,6 @@
 export PATH
 addr_line=`adb shell ifconfig | grep "inet addr:.* Bcast:"`
 addr=`echo ${addr_line} | sed 's/.*inet addr:\(.*\) B.*/\1/g'`
-echo $addr
 port=5555
 adb tcpip $port
-adb connect $addr
+adb connect ${addr}:${port}
